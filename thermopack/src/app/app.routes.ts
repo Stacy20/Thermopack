c90e19a40e0f7fd9dd8a022a3864489bc89b34c1
Alonso Garita <45644676+alonso-gg@users.noreply.github.com>
2024-02-12 13:26:42 -0600

    Carpetas
    Organización de las carpetas por admin, cliente y shared

--- thermopack/src/app/app.routes.ts
+++ thermopack/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './admin/pages/login-page/login-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'admin', //*Cambiar en caso de ser necesario
+    component: LoginPageComponent
+  }
+];
