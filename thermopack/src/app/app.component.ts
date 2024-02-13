51ddb1554dfc204fa80aee7c4847dfe4826f607f
Stacy20 <stacych30@estudiantec.cr>
2024-02-12 20:38:08 -0600

    Cards

--- thermopack/src/app/app.component.ts
+++ thermopack/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from "./shared/components/card/card.component";
+import { ListCardComponent } from "./shared/components/list-card/list-card.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, CardComponent, ListCardComponent]
 })
 export class AppComponent {
   title = 'thermopack';
---
+++ thermopack/src/app/shared/components/list-card/list-card.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CardComponent } from "../card/card.component";
+
+@Component({
+    selector: 'shared-list-card',
+    standalone: true,
+    templateUrl: './list-card.component.html',
+    styleUrl: './list-card.component.css',
+    imports: [CardComponent]
+})
+export class ListCardComponent {
+
+  public data: string[] = ['1','2','3'];
+}
