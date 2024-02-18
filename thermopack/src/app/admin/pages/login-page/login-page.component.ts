1bf1214c40d64f5c5c50063e3fd0221c9fc8a01c
Alonso Garita <45644676+alonso-gg@users.noreply.github.com>
2024-02-17 19:38:52 -0600

    Router en Login

--- thermopack/src/app/admin/pages/login-page/login-page.component.ts
+++ thermopack/src/app/admin/pages/login-page/login-page.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
   imports: [],
-  templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.css'
+  templateUrl: './login-page.component.html'
 })
 export class LoginPageComponent {
 
+  constructor(private router: Router) {}
+
+  submitUserLogin(): void {
+    //?Aquí se consume la API para la verificación y acreditación del usuario.
+
+    this.router.navigate(['/admin/config']);
+  }
 }
