227560f91267862ddd58db0cfac5da131509b95b
Alonso Garita <45644676+alonso-gg@users.noreply.github.com>
2024-02-19 13:23:16 -0600

    Config Contact Page

--- thermopack/src/app/admin/pages/config-contact-page/config-contact-page.component.ts
+++ thermopack/src/app/admin/pages/config-contact-page/config-contact-page.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
-import { UnderConstructionComponent } from "../../../shared/components/under-construction/under-construction.component";
+import { ConfigGalleryComponent } from "../../components/config-gallery/config-gallery.component";
 
 @Component({
     selector: 'app-config-contact-page',
     standalone: true,
     templateUrl: './config-contact-page.component.html',
-    styles: ``,
-    imports: [NavbarComponent, UnderConstructionComponent]
+    styles: `
+    .text-responsive {
+      text-align: left;
+      padding-top: 0.5rem;
+    }
+
+    @media (min-width: 770px) {
+      .text-responsive {
+        text-align: right;
+        padding: 0
+      }
+    }`,
+    imports: [NavbarComponent, ConfigGalleryComponent]
 })
 export class ConfigContactPageComponent {
 
