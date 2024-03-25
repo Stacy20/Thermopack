252a9e9c2d98100d0517700a841ef27f0ed25e28
Alonso Garita <45644676+alonso-gg@users.noreply.github.com>
2024-03-24 22:15:01 -0600

    Marcas, tipos, etc con paginación

--- thermopack/src/app/admin/components/edit-list/edit-list.component.ts
+++ thermopack/src/app/admin/components/edit-list/edit-list.component.ts
@@ -18,9 +18,40 @@ export class EditListComponent {
     public service: MainService,
   ) {}
 
-  @Input() items: ListItem[] = [];
+  public limitRows: number = 5;
+  public offset: number = 0;
+  public currentPage: number = 1;
+
+  private _items: ListItem[] = [];
+
+  @Input()
+  set items(value: ListItem[]) {
+    this._items = value;
+    this.updateRenderItems();
+  }
+  get items(): ListItem[] {
+    return this._items;
+  }
+
   @Output() edit = new EventEmitter<ListItem>();
   @Output() delete = new EventEmitter<ListItem>();
+
+  public renderItems: ListItem[] = [];
+
+  get totalRows(): number {
+    return this.items.length;
+  }
+
+  ngOnInit(): void {
+    this.updateRenderItems();
+  }
+
+  updateRenderItems(): void {
+    const startIndex = this.offset;
+    const endIndex = Math.min(startIndex + this.limitRows, this.items.length);
+    this.renderItems = this.items.slice(startIndex, endIndex);
+    console.log(startIndex, endIndex)
+  }
 
   editItem(item: ListItem) {
     const element = document.querySelector(`[data-index="${item._id}"]`);
@@ -30,7 +61,35 @@ export class EditListComponent {
   }
 
   deleteItem(item: ListItem) {
-    this.items = this.items.filter(iterItem => iterItem._id !== item._id);
+    this._items = this._items.filter(iterItem => iterItem._id !== item._id);
     this.delete.emit(item);
+    this.updateRenderItems(); // Actualizar renderItems después de eliminar un elemento
+  }
+
+  generatePagination(): number[] {
+    let totalPages = Math.ceil(this.totalRows / this.limitRows);
+    let startPage = 1;
+    const paginationItems: number[] = [];
+
+    if (totalPages > 3) {
+      if (this.currentPage > 1 && this.currentPage < totalPages) {
+        startPage = this.currentPage - 1;
+      } else if (this.currentPage === totalPages) {
+        startPage = totalPages - 2;
+      }
+    }
+
+    for (let i = 0; i < 3 && startPage <= totalPages; i++, startPage++) {
+      paginationItems.push(startPage);
+    }
+
+    return paginationItems;
+  }
+
+  updateCurrentPage(change: number): void {
+    this.currentPage += change;
+    this.offset = (this.currentPage - 1) * this.limitRows;
+  // Asegurarse de que offset se actualice antes de llamar a updateRenderItems
+    setTimeout(() => this.updateRenderItems());
   }
 }
