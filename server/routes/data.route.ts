8bd82d9c9d9388e2395dce21ef12c8ff8e3799ac
Stacy20 <stacych30@estudiantec.cr>
2024-06-16 17:32:56 -0600

    separar texto

--- server/routes/data.route.ts
+++ server/routes/data.route.ts
@@ -8,25 +8,70 @@ router.get('/', async (req, res) => {
     const data = await DataModel.find({}).lean().exec();
     res.status(200).json(data);
 });
-
-// Modifica la data
-router.put('/', async (req, res) => {
-  const data = await DataModel.findOneAndUpdate({}, { $set: {
-    slogan: req.body.slogan,
-    description: req.body.description,
-    mision: req.body.mision,
-    vision: req.body.vision,
-    logo: req.body.logo,
-    visionImages: req.body.visionImages,
-    presentationImages: req.body.presentationImages,
-    productsTitle: req.body.productsTitle,
-    productsParagraph: req.body.productsParagraph,
-    servicesTitle: req.body.servicesTitle,
-    servicesParagraph: req.body.servicesParagraph,
-  } }, { new: true });
-  if (!data) { return res.status(404).json({ message: 'No records found' });}
-  return res.status(202).json({ message: 'Successfully modified', data });
+// Obtiene todo el texto (misión, visión, slogan, etc.)
+router.get('/text', async (req, res) => {
+  const data = await DataModel.findOne({}, 'slogan description mision vision productsTitle productsParagraph servicesTitle servicesParagraph').lean().exec();
+  res.status(200).json(data);
 });
 
+// Obtiene el logo
+router.get('/logo', async (req, res) => {
+  const data = await DataModel.findOne({}, 'logo').lean().exec();
+  res.status(200).json(data);
+});
+
+// Obtiene las imágenes de visión
+router.get('/visionImages', async (req, res) => {
+  const data = await DataModel.findOne({}, 'visionImages').lean().exec();
+  res.status(200).json(data);
+});
+
+// Obtiene las imágenes de presentación
+router.get('/presentationImages', async (req, res) => {
+  const data = await DataModel.findOne({}, 'presentationImages').lean().exec();
+  res.status(200).json(data);
+});
+// // Modifica los textos
+// router.put('/texts', async (req, res) => {
+//   const data = await DataModel.findOneAndUpdate({}, { $set: {
+//     slogan: req.body.slogan,
+//     description: req.body.description,
+//     mision: req.body.mision,
+//     vision: req.body.vision,
+//     productsTitle: req.body.productsTitle,
+//     productsParagraph: req.body.productsParagraph,
+//     servicesTitle: req.body.servicesTitle,
+//     servicesParagraph: req.body.servicesParagraph,
+//   } }, { new: true });
+//   if (!data) { return res.status(404).json({ message: 'No records found' }); }
+//   return res.status(202).json({ message: 'Successfully modified', data });
+// });
+
+// // Modifica las imágenes de visión
+// router.put('/vision-images', async (req, res) => {
+//   const data = await DataModel.findOneAndUpdate({}, { $set: {
+//     visionImages: req.body.visionImages,
+//   } }, { new: true });
+//   if (!data) { return res.status(404).json({ message: 'No records found' }); }
+//   return res.status(202).json({ message: 'Successfully modified', data });
+// });
+
+// // Modifica las imágenes de presentación
+// router.put('/presentation-images', async (req, res) => {
+//   const data = await DataModel.findOneAndUpdate({}, { $set: {
+//     presentationImages: req.body.presentationImages,
+//   } }, { new: true });
+//   if (!data) { return res.status(404).json({ message: 'No records found' }); }
+//   return res.status(202).json({ message: 'Successfully modified', data });
+// });
+
+// // Modifica el logo
+// router.put('/logo', async (req, res) => {
+//   const data = await DataModel.findOneAndUpdate({}, { $set: {
+//     logo: req.body.logo,
+//   } }, { new: true });
+//   if (!data) { return res.status(404).json({ message: 'No records found' }); }
+//   return res.status(202).json({ message: 'Successfully modified', data });
+// });
 
 export default router;
--- thermopack/src/app/client/pages/home/home.component.ts
+++ thermopack/src/app/client/pages/home/home.component.ts
@@ -36,31 +36,70 @@ export class HomeComponent {
   formatDescription(description: string): string {
     return description.replace(/\n/g, '<br>');
   }
-  getData(): void {
-    this.service.getData().subscribe((data) => {
-      this.srcLogo = data[0].logo;
-      this.slogan = data[0].slogan;
-      this.companyDescription = data[0].description;
-      this.descriptionMission = data[0].mision;
-      this.descriptionVision = data[0].vision;
-      this.srcMission1 = data[0].visionImages[0];
-      this.srcMission2 =data[0].visionImages[1];
-      this.srcVision1 = data[0].visionImages[2];
-      this.srcVision2 = data[0].visionImages[3];
-      this.images = [];
-      data[0].presentationImages.forEach((imageString) => {
-        if (imageString != ''){
-          this.images.push(new ImageItem(
-            { src: imageString,
-            thumb: imageString},
-          ));
-        }
-      });
+  // getData(): void {
+  //   this.service.getData().subscribe((data) => {
+  //     this.srcLogo = data[0].logo;
+  //     this.slogan = data[0].slogan;
+  //     this.companyDescription = data[0].description;
+  //     this.descriptionMission = data[0].mision;
+  //     this.descriptionVision = data[0].vision;
+  //     this.srcMission1 = data[0].visionImages[0];
+  //     this.srcMission2 =data[0].visionImages[1];
+  //     this.srcVision1 = data[0].visionImages[2];
+  //     this.srcVision2 = data[0].visionImages[3];
+  //     this.images = [];
+  //     data[0].presentationImages.forEach((imageString) => {
+  //       if (imageString != ''){
+  //         this.images.push(new ImageItem(
+  //           { src: imageString,
+  //           thumb: imageString},
+  //         ));
+  //       }
+  //     });
+  //     if (this.checkDataLoaded()) {
+  //       this.loading = false;
+  //     }
+  //   });
+  // }
+  async getData(): Promise<void> {
+    try {
+      const data = await this.service.getTextData();
+      console.log(data);
+
+      if (data) {
+        this.slogan = data.slogan;
+        this.companyDescription = data.description;
+        this.descriptionMission = data.mision;
+        this.descriptionVision = data.vision;
+
+      }
+      const logo = await this.service.getLogo();
+      if (logo) {
+        this.srcLogo = logo.logo;
+      }
+      const visionImages = await this.service.getVisionImages();
+      if (visionImages) {
+        this.srcMission1 = visionImages.visionImages[0];
+        this.srcMission2 = visionImages.visionImages[1];
+        this.srcVision1 = visionImages.visionImages[2];
+        this.srcVision2 = visionImages.visionImages[3];
+      }
+
+      const presentationImages = await this.service.getPresentationImages();
+      if (presentationImages) {
+        this.images = presentationImages.presentationImages
+          .filter((imageString: string) => imageString !== '')
+          .map((imageString: string) => new ImageItem({ src: imageString, thumb: imageString }));
+      }
+
       if (this.checkDataLoaded()) {
         this.loading = false;
       }
-    });
+    } catch (error) {
+      console.error('Error al obtener los datos:', error);
+    }
   }
+
 
   checkDataLoaded(): boolean {
     return (
